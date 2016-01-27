6f0d2fba370a26890cff2e1b0b6e5d99dca566c4
Zoreslav Goral <Zoreslav.Goral@securenettech.com>
2016-01-27 18:22:24 +0200

    add tests

--- test/index.js
+++ test/index.js
@@ -2,18 +2,38 @@ var assert = require('assert');
 import countries from '../src/index.js';
     
 describe('Countries data', function(){
-	beforeEach(function() {
-		countries.init();
-	});
+    beforeEach(function() {
+        countries.init();
+    });
 
     it('should return default countries data', function(){
         assert.equal(countries.data.USA, 'United States of America');
         assert.equal(countries.data.UKR, 'Ukraine');
-	});
+    });
 
-	it('should return default countries data like object', function(){
+    it('should return default countries data like object', function(){
         assert.equal(countries.values().USA, 'United States of America');
         assert.equal(countries.values().UKR, 'Ukraine');
-	});
+    });
+
+    it('should return default countries data like array', function(){
+        assert.equal(countries.toArray().length, 249);
+    });
+
+    it('should transform to Alpha-2 format', function(){
+        assert.equal(countries.alpha2().values().UA, 'Ukraine');
+        assert.equal(countries.alpha2().values().US, 'United States of America');
+    });
+
+    it('should translate to Ukrainian', function(){
+        assert.equal(countries.translate('UKR').values().UKR, 'Україна');
+        assert.equal(countries.translate('UA').values().UKR, 'Україна');
+        assert.equal(countries.translate('ukr').values().UKR, 'Україна');
+        assert.equal(countries.translate('ua').values().UKR, 'Україна');
+    });
+
+    it('should not translate to', function(){
+        assert.equal(countries.translate('boo').values().UKR, 'Ukraine');
+    });
 
 });
