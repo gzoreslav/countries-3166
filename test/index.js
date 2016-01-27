2b746f77547359c3ec21cb314786b53eba0aad87
Zoreslav Goral <Zoreslav.Goral@securenettech.com>
2016-01-27 18:23:23 +0200

    add tests

--- test/index.js
+++ test/index.js
@@ -25,11 +25,12 @@ describe('Countries data', function(){
         assert.equal(countries.alpha2().values().US, 'United States of America');
     });
 
-    it('should translate to Ukrainian', function(){
+    it('should translate to', function(){
         assert.equal(countries.translate('UKR').values().UKR, 'Україна');
         assert.equal(countries.translate('UA').values().UKR, 'Україна');
         assert.equal(countries.translate('ukr').values().UKR, 'Україна');
         assert.equal(countries.translate('ua').values().UKR, 'Україна');
+        assert.equal(countries.translate('usa').values().UKR, 'США');
     });
 
     it('should not translate to', function(){
